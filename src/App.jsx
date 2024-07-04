8bd7ebafa3b342ec8a8052f86dc9574d3a294373
Tus-GitHub <tushardhull22@gmail.com>
2024-07-04 20:18:20 +0530

    First React Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from'react'
+import './index.css'
+import Navbar from './components/Navbar.jsx'
+import Body from './components/Body.jsx'
+
+
+function App() {
+
+  return (
+    <div className="container">
+            <Navbar />
+            <Body />
+      </div>
+    )
+}
+
+export default App
